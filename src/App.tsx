6d9866113e5d7403299c6e8201585adb669a9451
OisloAnalitycs <hello@oisloanalytics.com>
2025-08-29 18:02:56 +0200

    Adapt Cosmos Background to React Vite Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FloatingCosmosBackground from './components/FloatingCosmosBackground';
+import GridOverlay from './components/GridOverlay';
 import HomePage from './pages/HomePage';
 import ServicesPage from './pages/ServicesPage';
 import BlogPost from './pages/BlogPost';
@@ -10,7 +12,16 @@ import MasterclassPage from './pages/MasterclassPage';
 
 function App() {
   return (
-    <div className="min-h-screen bg-black">
+    <div className="min-h-screen bg-black relative">
+      {/* Fondo dinámico */}
+      <div className="fixed inset-0 -z-10">
+        <FloatingCosmosBackground density={150} parallax={0.035} connect={true} />
+        <GridOverlay />
+        {/* glow extra para el hero */}
+        <div className="pointer-events-none absolute inset-0" style={{
+          background: 'radial-gradient(600px 400px at 65% 30%, rgba(34, 197, 94, 0.18), transparent 70%)'
+        }} />
+      </div>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
